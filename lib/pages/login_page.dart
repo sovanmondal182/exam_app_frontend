131a4053b6210f6cf10c853372ac331fe0cafb5f
Sovan Mondal <sovanmondal182@gmail.com>
2022-11-23 23:43:26 +0530

    bug fixes

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -64,106 +64,121 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    DateTime timeBackPressed = DateTime.now();
+
     // _idController.text = "63736ebf5ab865e02d343134";
     // _idController.text = "637e023a8b676e059b66e310";
     // _passwordController.text = "12345678";
 
-    return Scaffold(
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 50),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/images/logo_app.png',
-                    scale: 1.5,
-                  ),
-                  SizedBox(height: 5),
-                  Text(
-                    "LOGIN",
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.w500,
-                      color: AppTheme.themeData.highlightColor,
+    return WillPopScope(
+      onWillPop: () async {
+        final difference = DateTime.now().difference(timeBackPressed);
+        final isExitWarning = difference.inSeconds > 2;
+        timeBackPressed = DateTime.now();
+        if (isExitWarning) {
+          AppUtils.showToast('Please back again to exit');
+          return false;
+        } else {
+          return true;
+        }
+      },
+      child: Scaffold(
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 50),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/logo_app.png',
+                      scale: 1.5,
                     ),
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 25.0,
-                right: 25.0,
-                bottom: 10.0,
-              ),
-              child: TextField(
-                key: Key(Strings.idFieldKey),
-                controller: _idController,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  prefixIcon: Icon(
-                    Icons.person,
-                    size: 30,
-                  ),
-                  label: Text(
-                    'Login ID',
-                  ),
-                  labelStyle: TextStyle(fontSize: 20),
-                  errorText: isIdValid ? null : Strings.idFieldError,
+                    SizedBox(height: 5),
+                    Text(
+                      "LOGIN",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w500,
+                        color: AppTheme.themeData.highlightColor,
+                      ),
+                    )
+                  ],
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 25.0, right: 25.0),
-              child: TextField(
-                key: Key(Strings.passwordFieldKey),
-                controller: _passwordController,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  prefixIcon: Icon(
-                    Icons.lock,
-                    size: 30,
-                  ),
-                  label: Text(
-                    Strings.passwordFieldHint,
-                  ),
-                  labelStyle: TextStyle(
-                    fontSize: 20,
-                  ),
-                  errorText:
-                      isPasswordValid ? null : Strings.passwordFieldError,
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 25.0,
+                  right: 25.0,
+                  bottom: 10.0,
                 ),
-                obscureText: true,
+                child: TextField(
+                  key: Key(Strings.idFieldKey),
+                  controller: _idController,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    prefixIcon: Icon(
+                      Icons.person,
+                      size: 30,
+                    ),
+                    label: Text(
+                      'Login ID',
+                    ),
+                    labelStyle: TextStyle(fontSize: 20),
+                    errorText: isIdValid ? null : Strings.idFieldError,
+                  ),
+                ),
               ),
-            ),
-            SizedBox(height: 25),
-            GestureDetector(
-              onTap: () => _loginPressed(context),
-              key: Key(Strings.loginButtonKey),
-              child: Container(
-                height: 50,
-                width: 300,
-                decoration: BoxDecoration(
-                  color: AppTheme.themeDataDark.highlightColor,
-                  borderRadius: BorderRadius.circular(10.0),
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+                child: TextField(
+                  key: Key(Strings.passwordFieldKey),
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      size: 30,
+                    ),
+                    label: Text(
+                      Strings.passwordFieldHint,
+                    ),
+                    labelStyle: TextStyle(
+                      fontSize: 20,
+                    ),
+                    errorText:
+                        isPasswordValid ? null : Strings.passwordFieldError,
+                  ),
+                  obscureText: true,
                 ),
-                child: Center(
-                  child: Text(
-                    Strings.loginButtonText,
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.white,
+              ),
+              SizedBox(height: 25),
+              GestureDetector(
+                onTap: () => _loginPressed(context),
+                key: Key(Strings.loginButtonKey),
+                child: Container(
+                  height: 50,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    color: AppTheme.themeDataDark.highlightColor,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Center(
+                    child: Text(
+                      Strings.loginButtonText,
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
